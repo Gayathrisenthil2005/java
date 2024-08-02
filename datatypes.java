e8c06e5fd55b8d9e6bb1c90125f86097e1ac8202
Gayathri.S <gayathri2005senthil@gmail.com>
2024-08-02 10:12:10 +0530

    Create datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,23 @@
+package javaproject;
+public class datatypes 
+{
+    public static void main(String[] args)
+    {
+        boolean b1 = true;
+        byte by = 100;
+        char ch = 'A';
+        short sh = 100;
+        int i = 1;
+        long l = 1000L;
+        float fl = 10.25F;
+        double dl = 20.25;
+        System.out.println("boolean " + b1);
+        System.out.println("byte  " + by);
+        System.out.println("char  " + ch);
+        System.out.println("short  " + sh);
+        System.out.println("int  1");
+        System.out.println("long   " + l);
+        System.out.println("float  " + fl);
+        System.out.println("double  " + dl);
+    }
+}
