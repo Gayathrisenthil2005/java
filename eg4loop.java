8912d05cbdaefee97cb11f87ba785f8fedbbf0f3
Gayathri.S <gayathri2005senthil@gmail.com>
2024-08-02 10:26:48 +0530

    Create eg4loop.java

---
+++ eg4loop.java
@@ -0,0 +1,12 @@
+package javaproject;
+public class eg4loop 
+{
+    public static void main(String[] args) 
+    {
+        for(int i = 1; i <= 5; ++i) 
+        {
+            int a = i * i * i;
+            System.out.println(a);
+        }
+    }
+}
