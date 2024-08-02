7ec154bdb2540145e90303137e72315f5039ae09
Gayathri.S <gayathri2005senthil@gmail.com>
2024-08-02 10:21:19 +0530

    Create eg1loop.java

---
+++ eg1loop.java
@@ -0,0 +1,12 @@
+package javaproject;
+public class eg1loop
+{
+    public static void main(String[] args) 
+    {
+        for(int i = 3; i <= 1000; i *= 3) 
+        {
+            System.out.println(i);
+        }
+
+    }
+}
