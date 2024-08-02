ce037ade8719d7afd0ef2c532af75802e2c183b9
Gayathri.S <gayathri2005senthil@gmail.com>
2024-08-02 10:46:24 +0530

    Create eg15loop.java

---
+++ eg15loop.java
@@ -0,0 +1,14 @@
+package javaproject;
+public class eg15loop
+{
+	public static void main(String args[])
+	{
+			for(int i=1;i<=15;i=i+1)
+			{
+				for(int j=i*2;j<=i*2;j=j+1)
+				{
+					System.out.println(j);
+				}
+			}
+	}
+}
