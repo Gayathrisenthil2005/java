4d22e1248003d25ed31cb7741ec82c30994f53e9
Gayathri.S <gayathri2005senthil@gmail.com>
2024-08-02 10:27:29 +0530

    Create eg5loop.java

---
+++ eg5loop.java
@@ -0,0 +1,13 @@
+package javaproject;
+public class eg5loop 
+{
+    public static void main(String[] args) 
+    {
+        for(int i = 0; i <= 10; ++i) 
+        {
+            double a = -10.0 - Math.pow(2.0, (double)i);
+            System.out.println(a);
+        }
+
+    }
+}
