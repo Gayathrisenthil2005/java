ed506b0ed668e7d7270717f56f3d9c9cd38e8571
Gayathri.S <gayathri2005senthil@gmail.com>
2024-08-02 10:52:32 +0530

    Create ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,22 @@
+package javaproject;
+public class ifelse
+{
+    public static void main(String args[])
+    {
+        int a = 10;
+        int b = 40;
+        int c = 30;
+        if (b > a && b > c)
+        {
+            if (b > c)
+            {
+                System.out.println("b is the second largest");
+            }
+            else
+            {
+                System.out.println("c is the second largest number");
+            }
+        }
+    }
+}
+
