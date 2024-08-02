a4905c8b875966248e400dc629fd4958da025b50
Gayathri.S <gayathri2005senthil@gmail.com>
2024-08-02 10:25:55 +0530

    Create eg3loop.java

---
+++ eg3loop.java
@@ -0,0 +1,12 @@
+package javaproject;
+public class eg3loop
+{
+public static void main(String[] args) 
+{
+        for(int i = 105; i >= 7; i -= 7) 
+        {
+            System.out.println(i);
+        }
+
+    }
+}
