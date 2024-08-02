aca0726b2847f40c1c3c3b11d1b8d121f3435f8a
Gayathri.S <gayathri2005senthil@gmail.com>
2024-08-02 10:47:16 +0530

    Create eg16loop.java

---
+++ eg16loop.java
@@ -0,0 +1,15 @@
+package javaproject;
+public class eg16loop
+{
+	public static void main(String args[])
+	{
+			for(int i=1;i<=10;i=i+1)
+			{
+				for(int j=1;j<=15;j=j+1)
+				{
+					System.out.print(j*i + " ");
+				}
+				System.out.println();
+			}
+	}
+}
