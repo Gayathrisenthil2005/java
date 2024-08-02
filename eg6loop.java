e7e51f378326d24cd302219f5d779ede95748964
Gayathri.S <gayathri2005senthil@gmail.com>
2024-08-02 10:28:41 +0530

    Create eg6loop.java

---
+++ eg6loop.java
@@ -0,0 +1,13 @@
+package javaproject;
+public class eg6loop
+{
+	public static void main(String args[])
+	{
+	int n =2;
+	for(int i=1;i<=5;i=i+1)
+	{
+		System.out.println(n);
+		n=n*10+2;
+	}
+	}
+}
