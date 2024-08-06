e6a3e2063d1190115c580d857be4396ce8f09e55
Gayathri.S <gayathri2005senthil@gmail.com>
2024-08-06 21:45:15 +0530

    Create do1.java

---
+++ do1.java
@@ -0,0 +1,14 @@
+package javaproject;
+public class do1
+{
+	public static void main(String args[])
+	{
+		int i=1;
+		do 
+		{
+			System.out.println("chennai");
+			i=i+1;
+		}
+		while(i<5);
+	}
+}
