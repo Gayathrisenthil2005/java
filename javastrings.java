c49917b6f6f1b35e789d17a57e78a42b852952c4
Gayathri.S <gayathri2005senthil@gmail.com>
2024-09-12 21:37:22 +0530

    Create javastrings.java

---
+++ javastrings.java
@@ -0,0 +1,15 @@
+package javaproject;
+public class javastrings
+{
+	public static void main(String[] args)
+	{
+		String a="hello";
+		String b="world";
+		for(int i=0;i<a.length();i=i+1)
+		{
+			System.out.print(a.charAt(i));
+		}
+
+	}
+
+}
