4e774195cfb72293e9997a2cf6d4b3963b5311ce
Gayathri.S <gayathri2005senthil@gmail.com>
2024-09-12 21:39:06 +0530

    Create examsubstring.java

---
+++ examsubstring.java
@@ -0,0 +1,11 @@
+package javaproject;
+public class examsubstring 
+{
+	public static void main(String[] args)
+	{
+		StringBuilder s1 = new StringBuilder("hello");
+		System.out.println(s1.substring(0,3));
+
+	}
+
+}
