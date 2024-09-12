c14e640ca58acc0ff7cc3f744021c13a8e5ef5ca
Gayathri.S <gayathri2005senthil@gmail.com>
2024-09-12 21:37:53 +0530

    Create stringexam1.java

---
+++ stringexam1.java
@@ -0,0 +1,12 @@
+package javaproject;
+public class stringexam1
+{
+	public static void main(String[] args)
+	{
+		String s=("hello");
+		s.concat("world");
+		System.out.print(s.concat("world"));
+
+	}
+
+}
