57b34348b8d8827174580076d43b906c571f4b7d
Gayathri.S <gayathri2005senthil@gmail.com>
2024-09-12 21:39:44 +0530

    Create examstringreverse.java

---
+++ examstringreverse.java
@@ -0,0 +1,15 @@
+package javaproject;
+public class examstringreverse 
+{
+	public static void main(String[] args)
+	{
+		String a="hello";
+		String b="world";
+		for(int i=4;i>=0;i=i-1)
+		{
+			System.out.print(a.charAt(i));
+		}
+
+	}
+}
+
