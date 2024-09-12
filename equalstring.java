51a898ebb33cc9306118992cf7f3700e21aeb842
Gayathri.S <gayathri2005senthil@gmail.com>
2024-09-12 21:38:25 +0530

    Create equalstring.java

---
+++ equalstring.java
@@ -0,0 +1,13 @@
+package javaproject;
+public class equalstring 
+{
+	public static void main(String[] args)
+	{
+		String s1=("hello");
+		String s2=("Hello");
+		System.out.println(s1.equals(s2));
+		System.out.print(s1.equalsIgnoreCase(s2));
+
+	}
+
+}
