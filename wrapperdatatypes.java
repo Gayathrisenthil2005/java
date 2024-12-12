a355e8c000070a05bd0de3b23298b08589c28913
Gayathri.S <gayathri2005senthil@gmail.com>
2024-12-12 19:24:44 +0530

    Create wrapperdatatypes.java

---
+++ wrapperdatatypes.java
@@ -0,0 +1,15 @@
+package java_course;
+public class wrapperdatatypes
+{
+	public static void main(String args[])
+	{
+		int a=20;
+		Integer a1=100;
+		a1.toString();
+		String s1="monkey"+a1;
+		System.out.println(s1);
+		System.out.println(a1.MAX_VALUE);
+		StringBuilder s2=new StringBuilder();
+		s2.append(a1.toString());
+	}
+}
