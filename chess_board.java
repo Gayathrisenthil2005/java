4faed402af4e27c1de0967c01d1ff83e1544f768
Gayathri.S <gayathri2005senthil@gmail.com>
2025-02-15 18:18:31 +0530

    Create chess_board.java

---
+++ chess_board.java
@@ -0,0 +1,295 @@
+package java_course;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class chess_board
+{
+	public static void main(String args[])
+	{
+		JFrame frame = new JFrame("chess Board");
+		frame.setSize(600,600);
+		frame.setLayout(new GridLayout(8,8));
+		
+		
+		JButton b1=new JButton();
+		b1.setBackground(Color.WHITE);
+		frame.add(b1);
+		
+		JButton b2=new JButton();
+		b2.setBackground(Color.BLACK);
+		frame.add(b2);
+		
+		JButton b3=new JButton();
+		b3.setBackground(Color.WHITE);
+		frame.add(b3);
+		
+		JButton b4=new JButton();
+		b4.setBackground(Color.BLACK);
+		frame.add(b4);
+		
+		JButton b5=new JButton();
+		b5.setBackground(Color.WHITE);
+		frame.add(b5);
+		
+		JButton b6=new JButton();
+		b6.setBackground(Color.BLACK);
+		frame.add(b6);
+		
+		JButton b7=new JButton();
+		b7.setBackground(Color.WHITE);
+		frame.add(b7);
+		
+		JButton b8=new JButton();
+		b8.setBackground(Color.BLACK);
+		frame.add(b8);
+		
+		JButton b9=new JButton();
+		b9.setBackground(Color.BLACK);
+		frame.add(b9);
+		
+		JButton b10=new JButton();
+		b10.setBackground(Color.WHITE);
+		frame.add(b10);
+		
+		JButton b11=new JButton();
+		b11.setBackground(Color.BLACK);
+		frame.add(b11);
+		
+		JButton b12=new JButton();
+		b12.setBackground(Color.WHITE);
+		frame.add(b12);
+		
+		JButton b13=new JButton();
+		b13.setBackground(Color.BLACK);
+		frame.add(b13);
+		
+		JButton b14=new JButton();
+		b14.setBackground(Color.WHITE);
+		frame.add(b14);
+		
+		JButton b15=new JButton();
+		b15.setBackground(Color.BLACK);
+		frame.add(b15);
+		
+		JButton b16=new JButton();
+		b16.setBackground(Color.WHITE);
+		frame.add(b16);
+		
+		JButton b17=new JButton();
+		b17.setBackground(Color.WHITE);
+		frame.add(b17);
+		
+		JButton b18=new JButton();
+		b18.setBackground(Color.BLACK);
+		frame.add(b18);
+		
+		JButton b19=new JButton();
+		b19.setBackground(Color.WHITE);
+		frame.add(b19);
+		
+		JButton b20=new JButton();
+		b20.setBackground(Color.BLACK);
+		frame.add(b20);
+		
+		JButton b21=new JButton();
+		b21.setBackground(Color.WHITE);
+		frame.add(b21);
+		
+		JButton b22=new JButton();
+		b22.setBackground(Color.BLACK);
+		frame.add(b22);
+		
+		JButton b23=new JButton();
+		b23.setBackground(Color.WHITE);
+		frame.add(b23);
+		
+		JButton b24=new JButton();
+		b24.setBackground(Color.BLACK);
+		frame.add(b24);
+		
+		JButton b25=new JButton();
+		b25.setBackground(Color.BLACK);
+		frame.add(b25);
+		
+		JButton b26=new JButton();
+		b26.setBackground(Color.WHITE);
+		frame.add(b26);
+		
+		JButton b27=new JButton();
+		b27.setBackground(Color.BLACK);
+		frame.add(b27);
+		
+		JButton b28=new JButton();
+		b28.setBackground(Color.WHITE);
+		frame.add(b28);
+		
+		JButton b29=new JButton();
+		b29.setBackground(Color.BLACK);
+		frame.add(b29);
+		
+		JButton b30=new JButton();
+		b30.setBackground(Color.WHITE);
+		frame.add(b30);
+		
+		JButton b31=new JButton();
+		b31.setBackground(Color.BLACK);
+		frame.add(b31);
+		
+		JButton b32=new JButton();
+		b32.setBackground(Color.WHITE);
+		frame.add(b32);
+		
+		JButton b33=new JButton();
+		b33.setBackground(Color.WHITE);
+		frame.add(b33);
+		
+		JButton b34=new JButton();
+		b34.setBackground(Color.BLACK);
+		frame.add(b34);
+		
+		JButton b35=new JButton();
+		b35.setBackground(Color.WHITE);
+		frame.add(b35);
+		
+		JButton b36=new JButton();
+		b36.setBackground(Color.BLACK);
+		frame.add(b36);
+		
+		JButton b37=new JButton();
+		b37.setBackground(Color.WHITE);
+		frame.add(b37);
+		
+		JButton b38=new JButton();
+		b38.setBackground(Color.BLACK);
+		frame.add(b38);
+		
+		JButton b39=new JButton();
+		b39.setBackground(Color.WHITE);
+		frame.add(b39);
+		
+		JButton b40=new JButton();
+		b40.setBackground(Color.BLACK);
+		frame.add(b40);
+		
+		JButton b41=new JButton();
+		b41.setBackground(Color.BLACK);
+		frame.add(b41);
+		
+		JButton b42=new JButton();
+		b42.setBackground(Color.WHITE);
+		frame.add(b42);
+		
+		JButton b43=new JButton();
+		b43.setBackground(Color.BLACK);
+		frame.add(b43);
+		
+		JButton b44=new JButton();
+		b44.setBackground(Color.WHITE);
+		frame.add(b44);
+		
+		JButton b45=new JButton();
+		b45.setBackground(Color.BLACK);
+		frame.add(b45);
+		
+		JButton b46=new JButton();
+		b46.setBackground(Color.WHITE);
+		frame.add(b46);
+		
+		JButton b47=new JButton();
+		b47.setBackground(Color.BLACK);
+		frame.add(b47);
+		
+		JButton b48=new JButton();
+		b48.setBackground(Color.WHITE);
+		frame.add(b48);
+		
+		JButton b49=new JButton();
+		b49.setBackground(Color.WHITE);
+		frame.add(b49);
+				
+		JButton b50=new JButton();
+		b50.setBackground(Color.BLACK);
+		frame.add(b50);
+		
+		JButton b51=new JButton();
+		b51.setBackground(Color.WHITE);
+		frame.add(b51);
+		
+		JButton b52=new JButton();
+		b52.setBackground(Color.BLACK);
+		frame.add(b52);
+		
+		JButton b53=new JButton();
+		b53.setBackground(Color.WHITE);
+		frame.add(b53);
+		
+		JButton b54=new JButton();
+		b54.setBackground(Color.BLACK);
+		frame.add(b54);
+		
+		JButton b55=new JButton();
+		b55.setBackground(Color.WHITE);
+		frame.add(b55);
+		
+		JButton b56=new JButton();
+		b56.setBackground(Color.BLACK);
+		frame.add(b56);
+		
+		JButton b57=new JButton();
+		b57.setBackground(Color.BLACK);
+		frame.add(b57);
+		
+		JButton b58=new JButton();
+		b58.setBackground(Color.WHITE);
+		frame.add(b58);
+		
+		JButton b59=new JButton();
+		b59.setBackground(Color.BLACK);
+		frame.add(b59);
+		
+		JButton b60=new JButton();
+		b60.setBackground(Color.WHITE);
+		frame.add(b60);
+		
+		JButton b61=new JButton();
+		b61.setBackground(Color.BLACK);
+		frame.add(b61);
+		
+		JButton b62=new JButton();
+		b62.setBackground(Color.WHITE);
+		frame.add(b62);
+		
+		JButton b63=new JButton();
+		b63.setBackground(Color.BLACK);
+		frame.add(b63);
+		
+		JButton b64=new JButton();
+		b64.setBackground(Color.WHITE);
+		frame.add(b64);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		frame.setVisible(true);
+		
+	}
+
+	private static void getScaledInstance(int i, int j, int scaleSmooth) {
+		// TODO Auto-generated method stub
+		
+	}
+}
