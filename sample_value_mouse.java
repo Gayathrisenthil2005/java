19a8b2c0c35b56ec92ccd173780bd390cce04759
Gayathri.S <gayathri2005senthil@gmail.com>
2025-02-15 18:36:04 +0530

    Create sample_value_mouse.java

---
+++ sample_value_mouse.java
@@ -0,0 +1,71 @@
+package java_course;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+public class sample_value_mouse 
+{
+    public static void main(String[] args)
+    {
+        JFrame f = new JFrame("Mouse Event Example");
+        f.setSize(600, 600);
+        f.setLayout(null);
+      
+        JTextField b1 = new JTextField();
+        b1.setBounds(50, 150, 100, 40); 
+        f.add(b1);
+
+        int width = b1.getWidth();
+        int height = b1.getHeight();
+        int x = b1.getX();
+        int y = b1.getY(); 
+        int a = x + width;
+        int b = y + height;
+        System.out.println("Top-left: " + x + "," + y);
+        System.out.println("Top-right: " + a + "," + y);
+        System.out.println("Bottom-left: " + x + "," + b);
+        System.out.println("Bottom-right: " + a + "," + b);
+
+        b1.addMouseListener(new MouseAdapter() 
+        {
+            @Override
+            public void mouseClicked(MouseEvent e) 
+            {
+                int clickX = e.getX() + b1.getX();
+                int clickY = e.getY() + b1.getY();
+                if (clickX >= x && clickX <= a && clickY >= y && clickY <= b) 
+                {
+                    System.out.println("Button Pressed");
+                }
+            }
+
+            public void mouseEntered(MouseEvent e) 
+            {
+            	 System.out.println("Mouse Entered");
+            }
+
+            public void mouseExited(MouseEvent e)
+            {
+            	 System.out.println("Mouse Exited");
+            }
+
+            public void mousePressed(MouseEvent e)
+            {
+            	int va=e.getX();
+            	int vb=e.getY();
+            	 System.out.println("button pressed" +va+ " "+vb);
+            }
+
+            public void mouseReleased(MouseEvent e) 
+            {
+            	 System.out.println("Mouse Released");
+            }
+
+        });
+
+        f.setVisible(true);
+    }
+}
