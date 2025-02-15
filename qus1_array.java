f33583eda89a325e1b2d159891dcfd241d03823c
Gayathri.S <gayathri2005senthil@gmail.com>
2025-02-15 18:29:08 +0530

    Create qus1_array.java

---
+++ qus1_array.java
@@ -0,0 +1,12 @@
+package java_course;
+public class qus1_array
+{
+	public static void main(String args[])
+	{
+		int []arr= {1,2,3,4,5,6,7,9};
+		for(int i=0;i<arr.length;i=i+1)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+}
