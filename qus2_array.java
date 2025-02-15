7c92d3617031b560aca254c1dbe04cd3699f1647
Gayathri.S <gayathri2005senthil@gmail.com>
2025-02-15 18:30:23 +0530

    Create qus2_array.java

---
+++ qus2_array.java
@@ -0,0 +1,13 @@
+package java_course;
+import java.util.Scanner;
+public class qus2_array
+{
+	public static void main(String args[])
+	{
+		int []arr= {1,3,4,6,7,10};
+		for(int i=5;i>=0;i--)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+}
